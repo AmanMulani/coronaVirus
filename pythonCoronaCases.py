f215111329b4eaf122571863731eb68e71611d6d
Aman Mulani <55574632+AmanMulani@users.noreply.github.com>
2020-03-20 17:31:35 +0530

    Comments added.

--- pythonCoronaCases.py
+++ pythonCoronaCases.py
@@ -2,7 +2,7 @@ import coronavirus as coniv
 import asyncio
 import time
 
-
+#This class is used to store the data of the country, whose details we want to find out.
 class StoreData():
 	def __init__(self, country, totalCases, recovered, deaths):
 		self.country = country
@@ -13,7 +13,7 @@ class StoreData():
 	def __str__(self):
 		return "Country: {}\nNo. of Cases: {}\nNo of patients recovered: {}\nTotal Number of deaths: {}".format(self.country, self.totalCases, self.recovered, self.deaths)
 
-
+	#calculate() is used to find the mortality and the recovery rate.
 	def calculate(self):
 		recoveryRate = self.recovered/self.totalCases * 100
 		mortalityRate = self.deaths/self.totalCases * 100
@@ -21,6 +21,7 @@ class StoreData():
 		print('Mortality Rate: {0}%'.format(mortalityRate))
 
 
+#connecting to the client server to fetch the information
 async def getCases():
 	print('Fetching data from server....')
 	session = coniv.ClientSession()
